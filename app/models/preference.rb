da5961ac26855ec59c7fb801d1b69387dd4a251a
José Pablo Alpaca Rivera <jose.p.alpaca@gmail.com>
2022-04-12 13:46:13 -0500

    fix validate title preference

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,5 +1,5 @@
 class Preference < ApplicationRecord
-  validates :opcion, presence: true,
+  validates :title, presence: true,
                      uniqueness: true
   # Associations
   has_and_belongs_to_many :users
