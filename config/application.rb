110a713322ed4f3e20d0435830b1645ae7aad811
Renato0496 <riar96@gmail.com>
2022-04-13 11:58:11 -0500

    added the config locales languages configuration

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,11 @@ module MicoletChallenge
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # setting languages translations
+
+    config.i18n.available_locales = [:en, :es, :fr]
+    config.i18n.default_locale = :en
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
